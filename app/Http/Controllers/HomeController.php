3418ee8bb98d72b3bf2bff9738dc6d15a1ecb01f
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2025-11-09 19:28:30 +0700

    init

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use Inertia\Inertia;
+
+
+class HomeController extends Controller
+
+{
+
+    public function home()
+
+    {
+
+        $data = [
+
+            'nama_lengkap' => 'Abdullah Ubaid',
+
+        ];
+
+        return Inertia::render('HomePage', $data);
+
+    }
+
+}
+
